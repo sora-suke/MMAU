237343027bd4d41b0caec7e439c197b19d3bd263
sora suke <mrkwsrw@yahoo.co.jp>
2016-11-23 12:05:42 +0900

    関係ないけどコミットメッセージ考えるの面倒くさい

--- java/com/sorasuke/MMAU/MMAU.java
+++ java/com/sorasuke/MMAU/MMAU.java
@@ -1,8 +1,6 @@
 package com.sorasuke.MMAU;
 
-import com.sorasuke.MMAU.blocks.MMAUBlocks;
 import com.sorasuke.MMAU.gens.MMAUGens;
-import com.sorasuke.MMAU.items.MMAUItems;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -16,26 +14,24 @@ public class MMAU {
 	public static final String MODID = "MoreMaterialsAndUtilities";
 	public static final String VERSION = "0.1.0";
 
-	public static final CreativeTabs MMAUTAB = new MMAUTab("mmautab");// クリエイティブタブ。
-	public static final CreativeTabs MMAUTOOLSTAB = new MMAUToolsTab("mmautoolstab");
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		
-		Logger.registry(event);//ロガーを生成
+		MMAULogger.registry(event);//ロガーを生成
 		
 		MMAUConfig.Load(this, event);// コンフィグの読み込み
 
-		MMAUItems.registry(this);// アイテムの登録
-		MMAUBlocks.registry(this);// ブロックの登録
+		MMAURegistry.registerMMAU();// ブロック､アイテムの登録等
+		MMAUOreDict.registry(this);// 鉱石辞書登録
 
 	}
 
 	@EventHandler
 	public void Init(FMLInitializationEvent event) {
 		
-		MMAUOreDict.registry(this);
-		Recipes.registry(this);// レシピ登録
+
+		MMAURecipeRegister.registry(this);// レシピ登録
 		MMAUGens.registry(this);// ワールド生成関連
 
 	}
