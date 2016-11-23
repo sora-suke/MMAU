18cae4b6f76347737355b563b0cfee59ba002f48
sora suke <mrkwsrw@yahoo.co.jp>
2016-11-23 11:43:56 +0900

    無機能アイテムベース

---
+++ java/com/sorasuke/MMAU/items/MMAUBaseItem.java
@@ -0,0 +1,22 @@
+package com.sorasuke.MMAU.items;
+
+import net.minecraft.item.Item;
+
+/**
+ * Created by sora_suke on 2016/11/19.
+ *
+ * MMAURegistry.registerItemで登録させるためのやつ
+ * コンストラクタの引数に名前を入れればその名前のテクスチャと非翻訳名にまとめて指定される
+ */
+public class MMAUBaseItem extends Item implements IMMAUBaseItem{
+    String name;
+    public MMAUBaseItem(String name){
+        this.name = name;
+        setUnlocalizedName("MMAU_" + name);
+        setTextureName("mmau:" + name);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
