fdbd501a8e16b42c1b35ff8d57e7594330befcf1
sora suke <mrkwsrw@yahoo.co.jp>
2016-11-23 11:34:57 +0900

    このMODで追加するブロックに共通で付けるインターフェース 専用のブロック登録メソッドで楽に登録できる

---
+++ java/com/sorasuke/MMAU/blocks/IMMAUBaseBlock.java
@@ -0,0 +1,13 @@
+package com.sorasuke.MMAU.blocks;
+
+/**
+ * Created by sora_suke on 2016/11/20.
+ *
+ * これを継承させればMMAURegistry.registerBlockでアイテム登録できる
+ * いちいちアイテム名入れて書いてGameRegistry書くのめんどくさかったから作った
+ * IMMAUBaseBlockを継承させれば名前もまとめて扱える
+ */
+public interface IMMAUBaseBlock {
+    String name = "";
+    String getName();
+}
