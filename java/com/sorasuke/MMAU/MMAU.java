017abc5cbf1ff358664828cfbe23145c23fda04c
sora suke <mrkwsrw@yahoo.co.jp>
2016-12-22 18:45:17 +0900

    プロキシの追加

--- java/com/sorasuke/MMAU/MMAU.java
+++ java/com/sorasuke/MMAU/MMAU.java
@@ -1,17 +1,21 @@
 package com.sorasuke.MMAU;
 
+import com.sorasuke.MMAU.common.MMAUProxy;
 import com.sorasuke.MMAU.gens.MMAUGens;
 
 import com.sorasuke.MMAU.tileentities.TileEntityChickenBlock;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.PlayerEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.event.entity.living.LivingFallEvent;
 
 @Mod(modid = MMAU.MODID, version = MMAU.VERSION)
@@ -21,6 +25,8 @@ public class MMAU {
 	public static final String VERSION = "0.1.0";
 	@Mod.Instance
 	public static MMAU instance;
+    @SidedProxy(clientSide = "com.sorasuke.MMAU.client.MMAUClientProxy", serverSide = "com.sorasuke.MMAU.common.MMAUProxy")
+    public static MMAUProxy proxy;
 
     //GUIのID
     public static final int guiIdChickenBlock = 0;
@@ -45,7 +51,7 @@ public class MMAU {
 	@EventHandler
 	public void Init(FMLInitializationEvent event) {
         NetworkRegistry.INSTANCE.registerGuiHandler(this.instance,new MMAUGuiHandler());
-
+        proxy.registerRender();
 		MMAURecipeRegister.registry(this);// レシピ登録
 		MMAUGens.registry(this);// ワールド生成関連
 
@@ -57,10 +63,17 @@ public class MMAU {
 	@SubscribeEvent
     public void fallToFeatherBlock(LivingFallEvent event){
         MMAULogger.log("fall!");
+        System.out.println("fall!");
+        //if(MMAURegistry.FeatherBlock.equals(event.entityLiving.worldObj.getBlock((int)event.entityLiving.posX, (int)event.entityLiving.posY-1, (int)event.entityLiving.posZ))){
+        //    event.setCanceled(true);
+        //}
+    }
 
-        if(MMAURegistry.FeatherBlock.equals(event.entityLiving.worldObj.getBlock((int)event.entityLiving.posX, (int)event.entityLiving.posY-1, (int)event.entityLiving.posZ))){
-            event.setCanceled(true);
-        }
+    @SubscribeEvent
+    public void onCraftedHook(PlayerEvent.ItemCraftedEvent event)
+    {
+        String craftedItemName = event.crafting.getDisplayName();
+        event.player.addChatComponentMessage(new ChatComponentText(craftedItemName + " has Crafted"));
     }
 
 }
