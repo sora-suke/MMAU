cb44dcd16149cd06c463eebb9c13339313b10982
sora suke <mrkwsrw@yahoo.co.jp>
2016-12-22 18:48:08 +0900

    ほぼからっぽ

---
+++ java/com/sorasuke/MMAU/tileentities/TileEntityChickenHead.java
@@ -0,0 +1,9 @@
+package com.sorasuke.MMAU.tileentities;
+
+import net.minecraft.tileentity.TileEntity;
+
+/**
+ * Created by sora_suke on 2016/12/22.
+ */
+public class TileEntityChickenHead extends TileEntity {
+}
