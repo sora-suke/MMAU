dedf3e5dd219fbff1bde9c7eea4b008a36a6cf31
sora suke <mrkwsrw@yahoo.co.jp>
2016-12-24 09:10:47 +0900

    念のため同期処理を入れた

--- java/com/sorasuke/MMAU/tileentities/TileEntityChickenHead.java
+++ java/com/sorasuke/MMAU/tileentities/TileEntityChickenHead.java
@@ -6,4 +6,9 @@ import net.minecraft.tileentity.TileEntity;
  * Created by sora_suke on 2016/12/22.
  */
 public class TileEntityChickenHead extends TileEntity {
+    public void updateEntity() {
+        worldObj.markBlockForUpdate(this.xCoord, this.yCoord, this.zCoord);//同期メソッド
+
+
+    }
 }
