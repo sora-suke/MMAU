ca6d959bce89075fdd3cf6623a280ec34b443521
sora suke <mrkwsrw@yahoo.co.jp>
2017-01-01 22:16:14 +0900

    余計なもの削除

--- java/com/sorasuke/MMAU/MMAU.java
+++ java/com/sorasuke/MMAU/MMAU.java
@@ -22,41 +22,40 @@ import net.minecraftforge.event.entity.living.LivingFallEvent;
 @Mod(modid = MMAU.MODID, version = MMAU.VERSION)
 public class MMAU {
 
-	public static final String MODID = "MoreMaterialsAndUtilities";
-	public static final String VERSION = "0.1.0";
-	@Mod.Instance
-	public static MMAU instance;
+    public static final String MODID = "MoreMaterialsAndUtilities";
+    public static final String VERSION = "0.1.0";
+    @Mod.Instance
+    public static MMAU instance;
     @SidedProxy(clientSide = "com.sorasuke.MMAU.client.MMAUClientProxy", serverSide = "com.sorasuke.MMAU.common.MMAUProxy")
     public static MMAUProxy proxy;
 
     //GUIのID
     public static final int guiIdChickenBlock = 0;
 
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		
-		MMAULogger.registry(event);//ロガーを生成
-		
-		MMAUConfig.Load(this, event);// コンフィグの読み込み
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        
+        MMAULogger.registry(event);//ロガーを生成
+        
+        MMAUConfig.Load(this, event);// コンフィグの読み込み
 
-		MMAURegistry.registerMMAU();// ブロック､アイテムの登録等
-		MMAUOreDict.registry(this);// 鉱石辞書登録
-
-
+        MMAURegistry.registerMMAU();// ブロック､アイテムの登録等
+        MMAUOreDict.registry(this);// 鉱石辞書登録
 
 
-	}
 
-	@EventHandler
-	public void Init(FMLInitializationEvent event) {
+
+    }
+
+    @EventHandler
+    public void Init(FMLInitializationEvent event) {
         NetworkRegistry.INSTANCE.registerGuiHandler(this.instance,new MMAUGuiHandler());
         proxy.registerRender();
-		MMAURecipeRegister.registry(this);// レシピ登録
-		MMAUGens.registry(this);// ワールド生成関連
-		//FMLCommonHandler.instance().bus().register(new MMAUEvents());
-		MinecraftForge.EVENT_BUS.register(new MMAUEvents());
+        MMAURecipeRegister.registry(this);// レシピ登録
+        MMAUGens.registry(this);// ワールド生成関連
+        MinecraftForge.EVENT_BUS.register(new MMAUEvents());
 
-	}
+    }
 
 
 
