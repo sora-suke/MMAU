646d403456b9046efa91a43189d48a00c1b06537
sora suke <mrkwsrw@yahoo.co.jp>
2017-01-18 19:48:25 +0900

    普通のProjectEのJarを放り込んだらエラー吐くから一時的にコメントアウト

--- java/com/sorasuke/MMAU/MMAUCollaboration.java
+++ java/com/sorasuke/MMAU/MMAUCollaboration.java
@@ -1,6 +1,6 @@
 package com.sorasuke.MMAU;
 
-import moze_intel.projecte.emc.mappers.APICustomEMCMapper;
+//import moze_intel.projecte.emc.mappers.APICustomEMCMapper;
 
 /**
  * Created by sora_suke on 2017/01/01.
@@ -9,11 +9,9 @@ public class MMAUCollaboration {
 
     public static void registerEMC() {
 
-        APICustomEMCMapper.instance.registerCustomEMC(MMAURegistry.Negi, 4096);
+        /*APICustomEMCMapper.instance.registerCustomEMC(MMAURegistry.Negi, 4096);
         APICustomEMCMapper.instance.registerCustomEMC(MMAURegistry.Crystal, 128);
         APICustomEMCMapper.instance.registerCustomEMC(MMAURegistry.Orichalcum, 65536);
-
-        APICustomEMCMapper.instance.registerCustomEMC(MMAURegistry.ChickenHead, 2048);
 
 
     }
