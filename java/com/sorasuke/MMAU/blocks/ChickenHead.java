df15f6f5b0b8743cd3f641d8f358e43f232134c4
sora suke <mrkwsrw@yahoo.co.jp>
2017-02-03 19:21:56 +0900

    音の種類の設定

--- java/com/sorasuke/MMAU/blocks/ChickenHead.java
+++ java/com/sorasuke/MMAU/blocks/ChickenHead.java
@@ -5,6 +5,7 @@ import com.sorasuke.MMAU.tileentities.TileEntityChickenHead;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.ITileEntityProvider;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockStateContainer;
@@ -40,6 +41,7 @@ public class ChickenHead extends BlockHorizontal implements IMMAUBaseBlock, ITil
         this.location = new ResourceLocation(MMAU.MODID, this.name);
         this.itemBlock = new ItemBlock(this);
         //this.setBlockBounds(5.5F / 16F, 0F, 5.5F / 16F, 10.5F / 16F, 6F / 16F, 10.5F / 16F);
+        setSoundType(SoundType.CLOTH);
     }
 
     @Override
