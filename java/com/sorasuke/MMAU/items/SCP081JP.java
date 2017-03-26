68d3f72e5e5cfd1f6239c29c72d16dc847e9db45
sora suke <mrkwsrw@yahoo.co.jp>
2017-03-26 13:36:33 +0900

    SCP-081-JPのツールチップとか

--- java/com/sorasuke/MMAU/items/SCP081JP.java
+++ java/com/sorasuke/MMAU/items/SCP081JP.java
@@ -1,8 +1,11 @@
 package com.sorasuke.MMAU.items;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
 import com.sorasuke.MMAU.MMAU;
 import com.sorasuke.MMAU.MMAURegistry;
 import net.minecraft.block.Block;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
@@ -11,7 +14,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -31,6 +37,13 @@ public class SCP081JP extends ItemArmor implements IMMAUBaseItem {
         setCreativeTab(MMAURegistry.MMAUToolsTab);
         setUnlocalizedName("MMAU_" + this.name);
         this.location = new ResourceLocation(MMAU.MODID, this.name);
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack itemstack, EntityPlayer player, List list, boolean advanced) {
+        list.add(I18n.format("text.scp081jp.tooltip", null));
     }
 
     @Override
