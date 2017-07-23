46daf936c8df75cf0e0802c1d1de3e238aa63277
sora suke <mrkwsrw@yahoo.co.jp>
2017-07-23 22:47:42 +0900

    箱を表示するようにしたり 線の色赤くしたり ゴーグル付けた時に透けて見えるようにしたり

--- java/com/sorasuke/MMAU/render/QuarrySpecialRender.java
+++ java/com/sorasuke/MMAU/render/QuarrySpecialRender.java
@@ -2,15 +2,20 @@ package com.sorasuke.MMAU.render;
 
 import com.sorasuke.MMAU.MMAU;
 import com.sorasuke.MMAU.MMAULogger;
+import com.sorasuke.MMAU.MMAURegistry;
+import com.sorasuke.MMAU.items.InformationGoggles;
 import com.sorasuke.MMAU.tileentities.TileEntityQuarry;
+import com.sorasuke.MMAU.utils.MMAUUtils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL45;
 
@@ -30,12 +35,14 @@ public class QuarrySpecialRender extends TileEntitySpecialRenderer<TileEntityQua
 
 
         if (0 < destroyStage) return;
+        if(!tileEntity.isRemembering) return;
         //GL11.glDisable(GL11.GL_LIGHTING);
         GlStateManager.pushMatrix();
         GlStateManager.disableCull();
         GlStateManager.disableLighting();//ここやるとむっちゃ明るくなる あかるくするかくらくするか...
-        GlStateManager.disableDepth();
-        GlStateManager.translate(x + 0.5F, y, z + 0.5F);
+        if((Minecraft.getMinecraft().thePlayer.getItemStackFromSlot(EntityEquipmentSlot.HEAD) != null)
+                && (Minecraft.getMinecraft().thePlayer.getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() instanceof InformationGoggles))GlStateManager.disableDepth();
+        GlStateManager.translate(x + 0.5F, y + 0.5F, z + 0.5F);
         //GlStateManager.enableBlend();
 
         //GlStateManager.enableLight(1);
@@ -46,17 +53,60 @@ public class QuarrySpecialRender extends TileEntitySpecialRenderer<TileEntityQua
         VertexBuffer wr = tessellator.getBuffer();
 
         this.bindTexture(texture);
-        wr.begin(GL11.GL_TRIANGLES, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
 
-
-        int color = Color.yellow.getRGB();
+        int color = Color.red.getRGB();
         int r = color >> 16 & 255;
         int g = color >> 8 & 255;
         int b = color & 255;
 
-        wr.pos(-30, 1, 2).normal(-30, 1, 2).color(r, g, b, 255).tex(0,1).endVertex();
-        wr.pos(0, 5, 0).normal(0, 5, 0).color(r, g, b, 255).tex(0,1).endVertex();
-        wr.pos(4, 1, 0).normal(4, 1, 0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
+
+        BlockPos ppp = MMAUUtils.getRelativePos(tileEntity.getPos(), tileEntity.getDiagonalPos());
+        int px = ppp.getX();
+        int py = ppp.getY();
+        int pz = ppp.getZ();
+
+        //wr.pos(0,0,0).normal(0,0,0).color(r, g, b, 255).tex(0,1).endVertex();
+        //wr.pos(px, py, pz).normal(px, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0,0,0).normal(0,0,0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, 0, 0).normal(px, 0, 0).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0,0,0).normal(0,0,0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(0, py, 0).normal(0, py, 0).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0,0,0).normal(0,0,0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(0, 0, pz).normal(0, 0, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(px, 0, 0).normal(px, 0, 0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, py, 0).normal(px, py, 0).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0, py, 0).normal(0, py, 0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, py, 0).normal(px, py, 0).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(px, 0, 0).normal(px, 0, 0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, 0, pz).normal(px, 0, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0, py, 0).normal(0, py, 0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(0, py, pz).normal(0, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(px, py, 0).normal(px, py, 0).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, py, pz).normal(px, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0, 0, pz).normal(0, 0, pz).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, 0, pz).normal(px, 0, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(px, 0, pz).normal(px, 0, pz).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, py, pz).normal(px, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0, 0, pz).normal(0, 0, pz).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(0, py, pz).normal(0, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+        wr.pos(0, py, pz).normal(0, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+        wr.pos(px, py, pz).normal(px, py, pz).color(r, g, b, 255).tex(0,1).endVertex();
+
+
+
 
         tessellator.draw();
 
